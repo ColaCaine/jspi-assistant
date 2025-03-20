b04f2b60ca565d069ff523f3c1a4aeaf72b45d8b
AvaCaine <createchivity@gmail.com>
2025-03-20 08:43:46 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,46 @@
-const cacheName = 'my-pwa-cache-v1'; // Name of the cache
-const assetsToCache = [ // Files to cache
-  '/', // Cache the root
+const CACHE_NAME = 'jspi-cache-v1';
+const urlsToCache = [
+  '/',
   '/index.html',
-  '/style.css', // If you have a CSS file
-  '/script.js', // If you have a JavaScript file
-  '/images/icon-192x192.png', // Example icon
-  '/images/icon-512x512.png' // Example icon
+  '/styles.css',
+  '/scripts.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
 ];
 
-// Install event: Caches the assets
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName)
+    caches.open(CACHE_NAME)
       .then(cache => {
         console.log('Opened cache');
-        return cache.addAll(assetsToCache);
+        return cache.addAll(urlsToCache);
       })
   );
 });
 
-// Fetch event: Serves cached assets or fetches from the network
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache hit - return response
         if (response) {
           return response;
         }
-        return fetch(event.request); // Fetch from network if not in cache
+        return fetch(event.request);
       })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
