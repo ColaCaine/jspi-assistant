0faf99c6b046312f671aee96b96c0c7ac66bd292
AvaCaine <createchivity@gmail.com>
2025-03-20 08:52:51 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,8 @@ const urlsToCache = [
   '/',
   '/index.html',
   '/styles.css',
-  '/scripts.js',
-  '/icons/icon-192x192.png',
+  '/service-worker.js',
+  '/jspi.png',
   '/icons/icon-512x512.png'
 ];
 
